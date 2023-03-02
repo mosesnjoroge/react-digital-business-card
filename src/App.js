ab1e630b709e891f4b0245f6f54d6bd5f6e5cb50
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-02 06:11:19 -0500

    component generation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Info from './components/Info';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Info />
       <About />
+      <Footer />
     </div>
   );
 }
