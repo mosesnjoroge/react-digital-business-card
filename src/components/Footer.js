998d7d908db15b1af2dbbe10d8f599c55f9a1ff4
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-03 20:20:18 -0500

    icons centered

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { faGithub, faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
   return (
-    <div>
+    <div className = 'footer-links'>
       <a href="https://github.com/mosesnjoroge"><FontAwesomeIcon icon={faGithub} /></a>
       <a href="https://www.linkedin.com/in/moses-njoroge-a78b46127/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
     </div>
