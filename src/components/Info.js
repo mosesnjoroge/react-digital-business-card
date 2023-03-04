4448a599606c362ba613946c9d92a59f6f746750
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-04 15:50:07 -0500

    bootstrap component added for btn styling

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,15 @@
 import React from "react";
 import logo from './profilephoto.jpeg';
+import Button from 'react-bootstrap/Button';
+
 export default function Info() {
   return (
   <div className= 'profilephoto'>
     <img src={logo} alt="Moses Njoroge" />
+    <h2>Moses Njoroge</h2>
+    <h5>Software Developer</h5>
+    <Button variant="outline-primary">Primary</Button>
+    <Button variant="outline-secondary">Secondary</Button>
   </div>
   )
 }
