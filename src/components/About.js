dc3b33529e9988650b798e0fe94596b0d99f19cf
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-04 17:47:34 -0500

    app.js dynamic sizing done

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className = 'aboutme'>
       <h2>About</h2>
-      <p> Recent graduate from Le Wagon's Web Development Coding Bootcamp. My jouney to love coding cmae about from taking the scenic route to the field I have always had an interest in. I'd describe myself as a person who is also curious and eager to learn, creative thinker always looking for efficiencies, and likes to take an initiative. I'm open to new opportunities and looking to utilise my previous experiences going forward in a new role as a Software developer.</p>
+      <p> My jouney to love coding came about from taking the scenic route to the field that I have always had an interest in. I'd describe myself as a person who is also curious and eager to learn, creative thinker always looking for efficiencies, and likes to take an initiative. I'm open to new opportunities and looking to utilise my previous experiences going forward in a new role as a Software developer.</p>
       <h2>Interests</h2>
       <p>I like vinyl collecting as i am an avid fan of music, and keeping active</p>
     </div>
